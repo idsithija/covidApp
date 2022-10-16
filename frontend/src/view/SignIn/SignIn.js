c51bd8ac67e71d1fa48dbb07c2a203de3a151171
idsithija <iidsithija@gmail.com>
2022-10-16 10:10:03 +0530

    changes

--- frontend/src/view/SignIn/SignIn.js
+++ frontend/src/view/SignIn/SignIn.js
@@ -1,9 +1,15 @@
 import React from "react";
+import SignInImg from "../../assets/img/login_bg.png";
 
 function SignIn() {
   return (
     <div className="vh-100 d-flex">
-      <div className="col-8 h-100"></div>
+      <div className="col-8 h-100 main-color login-p-l">
+        <div className="h-100 d-flex justify-content-center align-items-center flex-column">
+          <h2 className="title-color text-uppercase mb-5">Dashboard</h2>
+          <img className="sign-in-img mt-5" src={SignInImg} alt="signIn" />
+        </div>
+      </div>
       <div className="col-4 h-100"></div>
     </div>
   );
